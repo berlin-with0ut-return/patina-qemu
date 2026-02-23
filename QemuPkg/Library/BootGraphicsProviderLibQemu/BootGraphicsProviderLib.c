d7af2bdf186c81eb357a6043f7cd1ccf8b388667
Sherry Fan <sherryfan@microsoft.com>
2026-02-23 12:55:35 -0800

    cosmetic fixes

--- QemuPkg/Library/BootGraphicsProviderLibQemu/BootGraphicsProviderLib.c
+++ QemuPkg/Library/BootGraphicsProviderLibQemu/BootGraphicsProviderLib.c
@@ -4,7 +4,7 @@
 
   This instance uses defined PCDs and RAW FFS files
 
-  Copyright (C) Microsoft Corporation. All rights reserved.
+  Copyright (c) Microsoft Corporation. All rights reserved.
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
